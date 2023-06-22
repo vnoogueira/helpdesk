ec2ec2e180a9af7360e1513837b87f8bfd103b9a
Vitor Nogueira <vitor.nogueira94@gmail.com>
2023-06-22 12:30:36 -0300

    anotações JPA para classes de dominio

--- src/main/java/com/vitor/helpdesk/domain/Tecnico.java
+++ src/main/java/com/vitor/helpdesk/domain/Tecnico.java
@@ -3,18 +3,25 @@ package com.vitor.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.OneToMany;
+
+import com.vitor.helpdesk.domain.enums.Perfil;
+
 public class Tecnico extends Pessoa{
 	
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
-		// TODO Auto-generated constructor stub
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
-		// TODO Auto-generated constructor stub
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
