97db15b9d9e11039bcc812ac39d1bda941a7e8b4
Vitor Nogueira <vitor.nogueira94@gmail.com>
2023-06-23 17:08:08 -0300

    Implementando Tecnico DTO

--- src/main/java/com/vitor/helpdesk/services/TecnicoService.java
+++ src/main/java/com/vitor/helpdesk/services/TecnicoService.java
@@ -2,11 +2,13 @@ package com.vitor.helpdesk.services;
 
 import java.util.Optional;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vitor.helpdesk.domain.Tecnico;
 import com.vitor.helpdesk.domain.repositories.TecnicoRepository;
+import com.vitor.helpdesk.services.exceptions.ObjectNotFoundExceptions;
 
 @Service
 public class TecnicoService {
@@ -16,6 +18,6 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> tecnico = tecnicoRepository.findById(id);
-		return tecnico.orElse(null);
+		return tecnico.orElseThrow(() -> new ObjectNotFoundExceptions("Objeto não encontrado - ID: " + id));
 	}
 }
