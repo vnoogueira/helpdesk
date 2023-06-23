e0d0881beb213c8f366e1f53e30eca72cb0c82e9
Vitor Nogueira <vitor.nogueira94@gmail.com>
2023-06-22 23:19:43 -0300

    perfil de desenvolvimento criado

---
+++ src/main/java/com/vitor/helpdesk/config/DevConfig.java
@@ -0,0 +1,30 @@
+package com.vitor.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.vitor.helpdesk.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbservice;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
+	
+	@Bean
+	public boolean instanciaDb() {
+		if(value.equals("create")) {
+			this.dbservice.instaciaSB();
+		}
+		return false;
+	}
+
+}
