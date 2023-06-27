2ad638a1f96878f812fe7bbcf9e4a6448ba13eb7
Vitor Nogueira <vitor.nogueira94@gmail.com>
2023-06-26 23:48:10 -0300

    Criação EndPoint FindById (Cliente)

--- src/main/java/com/vitor/helpdesk/resources/ClienteResource.java
+++ src/main/java/com/vitor/helpdesk/resources/ClienteResource.java
@@ -3,9 +3,12 @@ package com.vitor.helpdesk.resources;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +30,12 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(clienteDTO);
 		
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClienteDTO> findById(@PathVariable Integer id){
+		Cliente obj = clienteService.findById(id);
+		return ResponseEntity.ok().body(new ClienteDTO(obj));
+	}
+	
 
 }
--- src/main/java/com/vitor/helpdesk/services/ClienteService.java
+++ src/main/java/com/vitor/helpdesk/services/ClienteService.java
@@ -1,12 +1,14 @@
 package com.vitor.helpdesk.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vitor.helpdesk.domain.Cliente;
 import com.vitor.helpdesk.domain.repositories.ClienteRepository;
+import com.vitor.helpdesk.services.exceptions.ObjectNotFoundExceptions;
 
 @Service
 public class ClienteService {
@@ -17,6 +19,13 @@ public class ClienteService {
 
 	public List<Cliente> findAll() {
 		return clienteRepository.findAll();
+	}
+
+
+	public Cliente findById(Integer id) {
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		
+		return cliente.orElseThrow(() -> new ObjectNotFoundExceptions("Cliente não encontrado ! - ID: " + id));
 	}
 
 }
